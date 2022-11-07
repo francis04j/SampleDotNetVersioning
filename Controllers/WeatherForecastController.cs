886a73cdf46a1fffa63b67a05cb74a257ffe53f9
Isreal369a <isreal369a@gmail.com>
2022-11-07 08:25:06 +0000

    feat: remove saltering

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Chilly", "Cool", "Mild", "Warm", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Chilly", "Cool", "Mild", "Warm", "Hot", "Scorching"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
